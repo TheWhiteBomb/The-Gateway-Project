9e22db8d027735232f44b1ff6fbaaf663bf91230
TheWhiteBomb <Robbyobryan58@gmail.com>
2024-10-01 15:44:46 -0600

    started C++

---
+++ Source/The_Gateway_Project/The_Gateway_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "The_Gateway_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, The_Gateway_Project, "The_Gateway_Project" );
