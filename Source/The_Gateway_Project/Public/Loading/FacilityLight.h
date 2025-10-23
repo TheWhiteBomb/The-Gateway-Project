04ed4d940ed94bcbaf36ec183c03a6dce83995dd
StaralDev <staralonyt@gmail.com>
2025-10-23 08:36:23 -0600

    Facility Lights

---
+++ Source/The_Gateway_Project/Public/Loading/FacilityLight.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FacilityLight.generated.h"
+
+UCLASS()
+class THE_GATEWAY_PROJECT_API AFacilityLight : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFacilityLight();
+
+	UFUNCTION(BlueprintCallable, Category = "Visibility")
+	void SetEnabled(const bool Enabled) const;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category="Visibility")
+	TArray<UMaterial*> Materials;
+
+	UPROPERTY(EditAnywhere, Category="Visibility")
+	UStaticMesh* Mesh;
+	
+private:
+	UPROPERTY()
+	USceneComponent* Root;
+
+	UPROPERTY()
+	UStaticMeshComponent* MeshComponent;
+};
